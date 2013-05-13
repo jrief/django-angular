01513db3a2660bd30d5ee03c9fb3d120b8123041
Jacob Rief <jacob.rief@gmail.com>
2013-05-13 16:39:18 +0200

    Fixed setup.py to be usable for a Python package

--- setup.py
+++ setup.py
@@ -1,9 +1,35 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+import djangular
 
+DESCRIPTION = 'Mixins classes and helper functions which help to integrate AngularJS with Django.'
 
-setup(name='djangular',
-      version='0.1.1',
-      packages=[],
-      license='MIT',
-      long_description="Mixins classes and helper functions which help to integrate AngularJS with Django.",
+try:
+    LONG_DESCRIPTION = open('README.md').read()
+except:
+    pass
+
+CLASSIFIERS = [
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
+setup(
+    name='django-angular',
+    version=djangular.__version__,
+    author='Jacob Rief',
+    author_email='jacob.rief@gmail.com',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    url='https://github.com/jrief/django-angular',
+    license='MIT',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    install_requires=['Django>=1.4'],
+    packages=find_packages(exclude=["tests", "example"]),
+    include_package_data=True,
 )
