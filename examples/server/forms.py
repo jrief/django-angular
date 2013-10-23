274dedc3a13067dd4c4df7ca54a7b560641a9643
Jacob Rief <jacob.rief@gmail.com>
2013-10-23 22:37:36 +0200

    Unit tests without NgModelForm

--- examples/server/forms.py
+++ examples/server/forms.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from django import forms
-from djangular.forms import NgModelFormMixin, NgFormValidationMixin
+from djangular.forms import NgFormValidationMixin
 
 
-class AdultSubscriptionForm(NgModelFormMixin, NgFormValidationMixin, forms.Form):
+class AdultSubscriptionForm(NgFormValidationMixin, forms.Form):
     first_name = forms.CharField(label='First name', min_length=3, max_length=20)
     middle_name = forms.CharField(label='Middle name', required=False)
     last_name = forms.RegexField(r'^[A-Z][a-z]+', label='Last name')
