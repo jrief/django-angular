9275811ac1b2e15a8a27e19649a56f1ca2c732db
Jacob Rief <jacob.rief@gmail.com>
2013-11-08 08:51:42 +0100

    Replaced Django-1.4 by Django-1.6 in testing matrix

--- examples/server/tests/__init__.py
+++ examples/server/tests/__init__.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-from django import VERSION
 from forms import *
 from views import *
-if VERSION[0] == 1 and VERSION[1] >= 5:
-    from validation import *
+from validation import *
