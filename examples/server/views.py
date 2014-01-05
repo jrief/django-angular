622edf8bb4c80fb8dcdd188dbc4529f1daeddcf5
Jacob Rief <jacob.rief@gmail.com>
2014-01-06 00:58:53 +0100

    Menu for 3-way databinding is available only in ws4redis context

--- examples/server/views.py
+++ examples/server/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.views.generic.base import TemplateView
+from django.conf import settings
 from server.forms import SubscriptionForm, SubscriptionFormWithNgModel
 
 
@@ -7,9 +8,13 @@ class NgFormValidationView(TemplateView):
     template_name = 'subscribe-form.html'
     form = SubscriptionForm(form_name='subscribe_form')
 
+    def __init__(self, **kwargs):
+        super(NgFormValidationView, self).__init__(**kwargs)
+        self.form.fields['height'].widget.attrs['step'] = 0.05  # Ugly hack to set step size
+
     def get_context_data(self, **kwargs):
         context = super(NgFormValidationView, self).get_context_data(**kwargs)
-        context.update(form=self.form)
+        context.update(form=self.form, with_ws4redis=hasattr(settings, 'WEBSOCKET_URL'))
         return context
 
 
