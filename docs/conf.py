8642a1934f4042eaa7048b383c06d37ae87a4de8
Jacob Rief <jacob.rief@gmail.com>
2014-01-06 23:42:10 +0100

    We're in 2014

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'django-angular'
-copyright = u'2013, Jacob Rief'
+copyright = u'2014, Jacob Rief'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
