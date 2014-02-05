74c9f16745cebd4697dea845f66d1a689d2bab72
Jacob Rief <jacob.rief@gmail.com>
2014-02-05 15:31:17 +0100

    removed debugging statement

--- examples/server/static/demo.js
+++ examples/server/static/demo.js
@@ -5,9 +5,9 @@
 angular.module('djangular-demo', ['ng.django.websocket'])
 .config(function(djangoWebsocketProvider) {
 	djangoWebsocketProvider.prefix('/ws');
+	djangoWebsocketProvider.debug(false);
 })
 .controller('MyFormController', function($scope, djangoWebsocket) {
-	console.log(djangoWebsocket); console.log($scope); console.log($scope.subscribe_data);
 	djangoWebsocket.connect($scope, ['subscribe-broadcast', 'publish-broadcast'], 'subscribe_data');
 });
 
