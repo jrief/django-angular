5a32c025e698d063440d0c1b73191d3156574686
Jacob Rief <jacob.rief@gmail.com>
2014-03-13 17:37:04 +0100

    Simplified starter app

--- examples/manage.py
+++ examples/manage.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python
 import os
 import sys
-import traceback
 
-sys.path.insert(0, os.path.abspath('./../'))
+sys.path[0:0] = [os.path.abspath('..'), os.path.abspath('../../django-websocket-redis/')]
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 
     from django.core.management import execute_from_command_line
 
-    try:
-        execute_from_command_line(sys.argv)
-    except Exception as exception:
-        # since this response is sent to the PSP, catch errors locally
-        print('%s while performing request' % (exception.__str__()))
-        traceback.print_exc()
-        raise exception
+    execute_from_command_line(sys.argv)
