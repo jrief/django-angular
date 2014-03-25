9ad34e380e279bd4e3cfadd0682272de792166c4
Jacob Rief <jacob.rief@gmail.com>
2014-03-25 17:50:14 +0100

    Refactored Ajax handler into own function

--- examples/server/views.py
+++ examples/server/views.py
@@ -21,15 +21,18 @@ class SubscribeFormView(TemplateView):
 
     def post(self, request, **kwargs):
         if request.is_ajax():
-            in_data = json.loads(request.body)
-            form = self.form(data=in_data)
-            response_data = {'errors': form.errors}
-            return HttpResponse(json.dumps(response_data), content_type="application/json")
+            return self.ajax(request.body)
         form = self.form(request.POST)
         if form.is_valid():
             return redirect('form_data_valid')
         context = self.get_context_data(form=form, **kwargs)
         return self.render_to_response(context)
+
+    def ajax(self, request_body):
+        in_data = json.loads(request_body)
+        form = self.form(data=in_data)
+        response_data = {'errors': form.errors}
+        return HttpResponse(json.dumps(response_data), content_type="application/json")
 
 
 class SubscribeViewWithFormValidation(SubscribeFormView):
