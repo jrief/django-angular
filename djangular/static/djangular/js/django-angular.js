6330f59cb1e08e2a9137eba0b784fc9fd3795c5d
Jacob Rief <jacob.rief@gmail.com>
2014-03-27 12:17:29 +0100

    Moved date field validator from example code to django-angular.js

--- djangular/static/djangular/js/django-angular.js
+++ djangular/static/djangular/js/django-angular.js
@@ -10,7 +10,12 @@
 'use strict';
 
 // Correct Angular's form.FormController behavior after rendering bound forms.
-angular.module('ng.django.forms', []).directive('form', function() {
+var djng_forms = angular.module('ng.django.forms', []);
+
+// This directive overrides some of the internal behavior on forms if used together with AngularJS.
+// If not used, the content of bound forms is not displayed, because AngularJS does not know about
+// the concept of bound forms.
+djng_forms.directive('form', function() {
 	return {
 		restrict: 'E',
 		scope: 'isolate',
@@ -28,7 +33,52 @@ angular.module('ng.django.forms', []).directive('form', function() {
 			form.$setPristine();
 		}
 	};
-}).provider('djangoForm', function() {
+});
+
+// This directive can be added to an input field which shall validate inserted dates, for example:
+// <input ng-model="a_date" type="text" validate-date="^(\d{4})-(\d{1,2})-(\d{1,2})$" />
+// Now, such an input field is only considered valid, if the date is a valid date and if it matches
+// against the given regular expression.
+djng_forms.directive('validateDate', function() {
+	var validDatePattern = null;
+
+	function validateDate(date) {
+		var matched, dateobj;
+		if (!date) // empty field are validated by the "required" validator
+			return true;
+		dateobj = new Date(date);
+		matched = validDatePattern ? Boolean(date.match(validDatePattern)) : true;
+		return matched && !isNaN(dateobj);
+	}
+
+	return {
+		require: 'ngModel',
+		restrict: 'A',
+		link: function(scope, elem, attrs, controller) {
+			if (attrs.validateDate) {
+				// if a pattern is set, only valid dates with that pattern are accepted
+				validDatePattern = new RegExp(attrs.validateDate, 'g');
+			}
+
+			// watch for modifications on input fields containing attribute 'validate-date="/pattern/"'
+			scope.$watch(attrs.ngModel, function(date) {
+				if (controller.$pristine)
+					return;
+				controller.$setValidity('date', validateDate(date));
+			});
+		}
+	}
+});
+
+// If forms are validated using Ajax, the server shall return a dictionary of detected errors to the
+// client code. The success-handler of this Ajax call, now can set those error messages on their
+// prepared list-items. The simplest way, is to add this code snippet into the controllers function
+// which is responsible for submitting form data using Ajax:
+//     $http.post("/path/to/url", $scope.data).success(function(data) {
+//         djangoForm.setErrors($scope.form, data.errors);
+//     });
+// djangoForm.setErrors returns false, if no errors have been transferred.
+djng_forms.provider('djangoForm', function() {
 	var NON_FIELD_ERRORS = '__all__';
 
 	function isNotEmpty(obj) {
