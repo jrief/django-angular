abfc402279a4abeba4397833372d8add583db2a1
Jacob Rief <jacob.rief@gmail.com>
2014-04-29 16:07:38 +0200

    using env to configure STATIC_ROOT

--- examples/server/settings.py
+++ examples/server/settings.py
@@ -41,7 +41,7 @@ MEDIA_URL = ''
 
 # Absolute path to the directory that holds static files.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.environ.get('DJANGO_STATIC_ROOT', '')
 
 # URL that handles the static files served from STATIC_ROOT.
 # Example: "http://media.lawrence.com/static/"
