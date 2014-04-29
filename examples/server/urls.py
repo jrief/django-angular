439e168110c5b78ca2bc4b446aae4768190c44da
Jacob Rief <jacob.rief@gmail.com>
2014-04-29 15:35:56 +0200

    Added extra text for external demo

--- examples/server/urls.py
+++ examples/server/urls.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url, patterns
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import RedirectView
 from server.views import (SubscribeViewWithFormValidation, SubscribeViewWithModelForm,
         SubscribeViewWithModelFormAndValidation, Ng3WayDataBindingView, NgFormDataValidView)
 
@@ -14,4 +16,5 @@ urlpatterns = patterns('',
     url(r'^threeway_databinding/$', Ng3WayDataBindingView.as_view(),
         name='djng_3way_databinding'),
     url(r'^form_data_valid', NgFormDataValidView.as_view(), name='form_data_valid'),
+    url(r'^$', RedirectView.as_view(url=reverse_lazy('djng_form_validation'))),
 )
