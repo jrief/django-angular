7e7de811719c202a7cc306a4b6f7f4785ef89049
Jacob Rief <jacob.rief@gmail.com>
2014-05-20 16:11:15 +0200

    Fixed #46

--- examples/server/forms.py
+++ examples/server/forms.py
@@ -18,6 +18,8 @@ class SubscriptionForm(forms.Form):
     last_name = forms.RegexField(r'^[A-Z][a-z -]?', label='Last name',
         error_messages={'invalid': 'Last names shall start in upper case'},
         help_text=u'The name ‘John Doe’ is rejected by the server.')
+    sex = forms.ChoiceField(choices=(('m', 'Male'), ('f', 'Female')),
+        error_messages={'invalid_choice': 'Please select your sex'})
     email = forms.EmailField(label='E-Mail', validators=[reject_addresses, validate_email],
         help_text=u'Addresses containing ‘@example’ are rejected by the server.')
     phone = forms.RegexField(r'^\+?[0-9 .-]{4,25}$', label='Phone number',
@@ -29,6 +31,8 @@ class SubscriptionForm(forms.Form):
         error_messages={'min_value': 'You are too lightweight'})
     height = forms.FloatField(min_value=1.48, max_value=1.95, label='Height in meters',
         error_messages={'max_value': 'You are too tall'})
+    annotation = forms.CharField(required=False, label='Annotation',
+        widget=forms.Textarea(attrs={'cols': '80', 'rows': '3'}))
 
     def clean(self):
         if self.cleaned_data.get('first_name') == 'John' and self.cleaned_data.get('last_name') == 'Doe':
