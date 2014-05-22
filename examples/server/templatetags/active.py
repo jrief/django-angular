d4454b65a84a83b46b7916fb97dfc28a40a4e082
Jacob Rief <jacob.rief@gmail.com>
2014-05-22 10:11:43 +0200

    Fixed: active atate for urlpath

--- examples/server/templatetags/active.py
+++ examples/server/templatetags/active.py
@@ -6,6 +6,11 @@ register = template.Library()
 
 @register.simple_tag
 def active(request, url):
-    if request.path.startswith(reverse(url)):
-        return 'active'
-    return ''
+    urlparts = reverse(url).lstrip('/').split('/')
+    requestparts = request.path.lstrip('/').split('/')
+    active = 'active'
+    for p1, p2 in zip(urlparts, requestparts):
+        if p1 != p2:
+            active = ''
+            break
+    return active
