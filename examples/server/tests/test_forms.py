4fdd6d1a016d319365b46d78cfa406e10e3dacb6
Jacob Rief <jacob.rief@gmail.com>
2014-06-06 11:44:45 +0200

    Additional unit tests for CheckboxSelectMultiple

--- examples/server/tests/test_forms.py
+++ examples/server/tests/test_forms.py
@@ -2,13 +2,14 @@
 import copy
 from django.db import models
 from django import forms
+from django.http import QueryDict
 from django.test import TestCase
 from djangular.forms import NgModelFormMixin, AddPlaceholderFormMixin
 from pyquery.pyquery import PyQuery
 from lxml import html
 
 
-CHOICES = (('a', 'Choice A'), ('b', 'Choice B'), ('c', 'Choice C'))
+CHOICES = (('a', 'Choice A'), ('b', 'Choice B'), ('c', 'Choice C'), ('d', 'Choice D'),)
 
 
 class SubModel(models.Model):
@@ -39,6 +40,9 @@ class InvalidForm(NgModelFormMixin, forms.ModelForm):
 class DummyForm(NgModelFormMixin, forms.Form):
     email = forms.EmailField(label='E-Mail')
     onoff = forms.BooleanField(initial=False, required=True)
+    sex = forms.ChoiceField(choices=(('m', 'Male'), ('f', 'Female')), widget=forms.RadioSelect)
+    select_multi = forms.MultipleChoiceField(choices=CHOICES)
+    check_multi = forms.MultipleChoiceField(choices=CHOICES, widget=forms.CheckboxSelectMultiple)
     scope_prefix = 'dataroot'
 
     def __init__(self, *args, **kwargs):
@@ -68,6 +72,9 @@ class NgModelFormMixinTest(TestCase):
     valid_data = {
         'email': 'john@example.com',
         'onoff': True,
+        'sex': 'f',
+        'select_multi': ['a', 'd'],
+        'check_multi': ['b', 'c'],
         'sub1': {
             'select_choices': 'c',
             'radio_choices': 'c',
@@ -97,37 +104,85 @@ class NgModelFormMixinTest(TestCase):
     def check_form_fields(self, form):
         for name in form.fields.keys():
             identifier = '%s.%s' % (form.prefix, name) if form.prefix else name
-            input_fields = [e for e in self.elements if e.name == identifier]
+            input_fields = [e for e in self.elements if e.name.startswith(identifier)]
             self.assertTrue(input_fields)
             for input_field in input_fields:
                 self.assertIsInstance(input_field, (html.InputElement, html.SelectElement))
                 self.assertEqual(input_field.attrib.get('ng-class'), 'fieldClass(\'%s\')' % identifier)
                 if identifier == 'sub2.radio_choices':
                     self.assertFalse(input_field.attrib.get('ng-model'))
+                elif identifier == 'check_multi':
+                    model = '%s.%s' % (self.unbound_form.scope_prefix, input_field.name)
+                    self.assertEqual(input_field.attrib.get('ng-model'), model)
                 else:
                     model = '%s.%s' % (self.unbound_form.scope_prefix, identifier)
                     self.assertEqual(input_field.attrib.get('ng-model'), model)
-                if isinstance(input_field, html.InputElement) and input_field.type == 'radio':
-                    if input_field.tail.strip() == CHOICES[1][1]:
-                        self.assertTrue(input_field.checked)
-                    else:
+                if isinstance(input_field, html.InputElement):
+                    if input_field.type == 'radio':
+                        if input_field.tail.strip() == CHOICES[1][1]:
+                            self.assertTrue(input_field.checked)
+                        else:
+                            self.assertFalse(input_field.checked)
+                    elif input_field.type == 'checkbox':
                         self.assertFalse(input_field.checked)
                 elif isinstance(input_field, html.SelectElement):
-                    self.assertListEqual(input_field.value_options, [c[0] for c in CHOICES])
-                    self.assertEqual(input_field.value, CHOICES[0][0])
+                    choices_keys = [c[0] for c in CHOICES]
+                    self.assertListEqual(input_field.value_options, choices_keys)
+                    if input_field.multiple:
+                        self.assertIsInstance(input_field.value, html.MultipleSelectOptions)
+                        choices_field = [o.values()[0] for o in input_field.value.options]
+                        self.assertListEqual(choices_field, choices_keys)
+                    else:
+                        self.assertEqual(input_field.value, CHOICES[0][0])
 
     def test_valid_form(self):
-        bound_form = DummyForm(data=self.valid_data)
+        query = QueryDict('')
+        query = query.copy()
+        in_data = copy.deepcopy(self.valid_data)
+        select_multi = in_data.pop('select_multi')
+        check_multi = in_data.pop('check_multi')
+        query.update(in_data)
+        query.setlist('select_multi', select_multi)
+        check_multi = dict(('check_multi.%s' % c, c) for c in check_multi)
+        query.update(check_multi)
+        bound_form = DummyForm(data=query)
         self.assertTrue(bound_form.is_bound)
         self.assertTrue(bound_form.is_valid())
 
-    def test_invalid_form(self):
+    def test_invalid_email(self):
         in_data = copy.deepcopy(self.valid_data)
         in_data['email'] = 'no.email.address'
         bound_form = DummyForm(data=in_data)
         self.assertTrue(bound_form.is_bound)
         self.assertFalse(bound_form.is_valid())
         self.assertTrue(bound_form.errors.pop('email', None))
+        self.assertFalse(bound_form.errors)
+
+    def test_invalid_sex(self):
+        in_data = copy.deepcopy(self.valid_data)
+        in_data['sex'] = 'X'
+        bound_form = DummyForm(data=in_data)
+        self.assertTrue(bound_form.is_bound)
+        self.assertFalse(bound_form.is_valid())
+        self.assertTrue(bound_form.errors.pop('sex', None))
+        self.assertFalse(bound_form.errors)
+
+    def test_invalid_select(self):
+        in_data = copy.deepcopy(self.valid_data)
+        in_data['select_multi'].append('X')
+        bound_form = DummyForm(data=in_data)
+        self.assertTrue(bound_form.is_bound)
+        self.assertFalse(bound_form.is_valid())
+        self.assertTrue(bound_form.errors.pop('select_multi', None))
+        self.assertFalse(bound_form.errors)
+
+    def test_invalid_checkmulti(self):
+        in_data = copy.deepcopy(self.valid_data)
+        in_data['check_multi'].append('X')
+        bound_form = DummyForm(data=in_data)
+        self.assertTrue(bound_form.is_bound)
+        self.assertFalse(bound_form.is_valid())
+        self.assertTrue(bound_form.errors.pop('check_multi', None))
         self.assertFalse(bound_form.errors)
 
     def test_invalid_subform(self):
