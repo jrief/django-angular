1646ec995dce76e73c8340a50c0dfc7a9a63f9f2
ndudenhoeffer <nate@dudesag.com>
2014-06-11 11:39:24 -0500

    Add grunt-contrib-watch

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -90,6 +90,12 @@ module.exports = function(grunt) {
 				reporters: ['dots'],
 				configFile: 'karma.conf.js'
 			}
+		},
+		watch: {
+		  client: {
+		    files: 'src/js/*.js',
+		    tasks: ['build'],
+		  }
 		}
 	});
 
@@ -97,6 +103,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-ngmin');
 	grunt.loadNpmTasks('grunt-karma');
 
