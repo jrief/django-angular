209194d644fe8f87f0393e7ced53e2b66f3b286f
ndudenhoeffer <nate@dudesag.com>
2014-06-11 07:32:14 -0500

    Fix typo in labe_suffix

--- djangular/forms/angular_base.py
+++ djangular/forms/angular_base.py
@@ -110,7 +110,7 @@ class NgBoundField(forms.BoundField):
         css_classes = getattr(self.field, 'label_css_classes', None)
         if css_classes:
             attrs.update({'class': css_classes})
-        self.forms.label_suffix = ''
+        self.form.label_suffix = ''
         return super(NgBoundField, self).label_tag(contents, attrs)
 
 
