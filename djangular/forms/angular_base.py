ecb1391da8ebcc354402dbca1eb1f2485b35192b
ndudenhoeffer <nate@dudesag.com>
2014-06-27 11:51:31 -0500

    revert master forms to match upstream

--- djangular/forms/angular_base.py
+++ djangular/forms/angular_base.py
@@ -110,8 +110,7 @@ class NgBoundField(forms.BoundField):
         css_classes = getattr(self.field, 'label_css_classes', None)
         if css_classes:
             attrs.update({'class': css_classes})
-        self.form.label_suffix = ''
-        return super(NgBoundField, self).label_tag(contents, attrs)
+        return super(NgBoundField, self).label_tag(contents, attrs, label_suffix='')
 
 
 class NgFormBaseMixin(object):
