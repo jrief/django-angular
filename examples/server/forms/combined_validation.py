d72836cf37918b3b1882d17f49063c6c2def02d6
Jacob Rief <jacob.rief@gmail.com>
2014-09-11 10:14:07 +0200

    Since password make a server-side validation, removed the email validation

--- examples/server/forms/combined_validation.py
+++ examples/server/forms/combined_validation.py
@@ -2,10 +2,7 @@
 from __future__ import unicode_literals
 # start tutorial
 from django.core.exceptions import ValidationError
-from django.core.validators import validate_email
-from django.forms import EmailField
 from djangular.forms import NgModelFormMixin, NgFormValidationMixin
-from .model_scope import reject_addresses
 from . import subscribe_form
 
 
@@ -13,11 +10,7 @@ class SubscribeForm(NgModelFormMixin, NgFormValidationMixin, subscribe_form.Subs
     scope_prefix = 'subscribe_data'
     form_name = 'my_form'
 
-    # Override the email field to add a server-side validator
-    email = EmailField(label='E-Mail', validators=[reject_addresses, validate_email],
-        help_text='Addresses containing ‘@example’ are rejected by the server.')
-
     def clean(self):
         if self.cleaned_data.get('first_name') == 'John' and self.cleaned_data.get('last_name') == 'Doe':
-            raise ValidationError('The full name \'John Doe\' is rejected by the server.')
+            raise ValidationError('The full name "John Doe" is rejected by the server.')
         return super(SubscribeForm, self).clean()
--- examples/server/forms/model_scope.py
+++ examples/server/forms/model_scope.py
@@ -1,29 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 # start tutorial
-from django.forms import EmailField
 from django.core.exceptions import ValidationError
-from django.core.validators import validate_email
 from djangular.forms import NgModelFormMixin
 from . import subscribe_form
-
-
-def reject_addresses(value):
-    """Reject email addresses ending with @example..."""
-    try:
-        value.lower().index('@example.')
-        raise ValidationError('Email address "{0}" is rejected by the server.'.format(value))
-    except ValueError:
-        pass
 
 
 class SubscribeForm(NgModelFormMixin, subscribe_form.SubscribeForm):
     scope_prefix = 'subscribe_data'
     form_name = 'my_form'
-
-    # Override the email field to add a server-side validator
-    email = EmailField(label='E-Mail', validators=[reject_addresses, validate_email],
-        help_text='Addresses containing "@example" are rejected by the server.')
 
     def clean(self):
         if self.cleaned_data.get('first_name') == 'John' and self.cleaned_data.get('last_name') == 'Doe':
