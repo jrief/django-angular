7e1bc4dab38b63c8fae8656c0916194364eb5008
jamesrobb <james@jamesrobb.com>
2014-11-26 21:03:14 +0000

    removed need for hardcoded rejected from msg_format_bind

--- djangular/forms/angular_messages.py
+++ djangular/forms/angular_messages.py
@@ -2,8 +2,7 @@ from django.utils.html import format_html
 from django.utils.encoding import force_text
 from django.utils.safestring import mark_safe, SafeText
 
-from djangular.forms.angular_base import TupleErrorList
-from djangular.forms.angular_base import NgFormBaseMixin
+from djangular.forms.angular_base import TupleErrorList, SafeTuple, NgFormBaseMixin
 
 
 class NgMessagesTupleErrorList(TupleErrorList):
@@ -11,7 +10,7 @@ class NgMessagesTupleErrorList(TupleErrorList):
     msgs_format = '<div class="{1}" ng-messages="{0}.$error" ng-show="{0}.$dirty && {0}.$invalid" ng-cloak>{2}</div>'
     msg_format = '<div ng-message="{1}" class="{2}">{3}</div>'
     """ span's necessary due to this bug https://github.com/angular/angular.js/issues/8089"""
-    msg_format_bind = '<div ng-message="rejected" class="{2}"><span ng-bind="{0}.{3}"></span></div>'
+    msg_format_bind = '<div ng-message="{1}" class="{2}"><span ng-bind="{0}.{3}"></span></div>'
 
     def as_ul(self):
         if not self:
@@ -20,7 +19,7 @@ class NgMessagesTupleErrorList(TupleErrorList):
         if isinstance(first, tuple):
             error_list = []
             for e in self:
-                if e[3] == '$valid':
+                if e[3] == '$valid' or e[2] == '$pristine':
                     continue
                 msg_format = e[5] == '$message' and self.msg_format_bind or self.msg_format
                 msg_type = e[3].split('.')
@@ -37,6 +36,14 @@ class NgMessagesMixin(NgFormBaseMixin):
     def __init__(self, data=None, *args, **kwargs):
         kwargs['error_class'] = NgMessagesTupleErrorList
         super(NgMessagesMixin, self).__init__(data, *args, **kwargs)
+
+    def get_field_errors(self, field):
+        errors = super(NgMessagesMixin, self).get_field_errors(field)
+        if field.is_hidden:
+            return errors
+        identifier = format_html('{0}.{1}', self.form_name, field.name)
+        errors.append(SafeTuple((identifier, self.field_error_css_classes, '$dirty', 'rejected', 'invalid', '$message')))
+        return errors
 
     def get_widget_attrs(self, bound_field):
         attrs = super(NgMessagesMixin, self).get_widget_attrs(bound_field)
