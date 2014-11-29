5a74e4552ee1cfa3ea3846266eeecbe8ea3043a2
jamesrobb <james@jamesrobb.com>
2014-11-29 20:24:28 +0000

    issue introduced with angular 1.3+ when using new ng-model-options debounce

--- examples/server/forms/combined_validation.py
+++ examples/server/forms/combined_validation.py
@@ -10,7 +10,14 @@ class SubscribeForm(NgModelFormMixin, NgFormValidationMixin, subscribe_form.Subs
     scope_prefix = 'subscribe_data'
     form_name = 'my_form'
 
+    def get_widget_attrs(self, bound_field):
+        attrs = super(SubscribeForm, self).get_widget_attrs(bound_field)
+        attrs['ng-model-options'] = "{ debounce : { 'default' : 500, blur : 0 } }"
+        return attrs
+
     def clean(self):
         if self.cleaned_data.get('first_name') == 'John' and self.cleaned_data.get('last_name') == 'Doe':
             raise ValidationError('The full name "John Doe" is rejected by the server.')
         return super(SubscribeForm, self).clean()
+
+
