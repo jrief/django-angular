a58569fb11f3917e55db27be109d3ba715d59f93
jamesrobb <james@jamesrobb.com>
2014-11-29 20:38:23 +0000

    calling $commitViewValue fix

--- examples/server/forms/combined_validation.py
+++ examples/server/forms/combined_validation.py
@@ -9,12 +9,12 @@ from . import subscribe_form
 class SubscribeForm(NgModelFormMixin, NgFormValidationMixin, subscribe_form.SubscribeForm):
     scope_prefix = 'subscribe_data'
     form_name = 'my_form'
-
+    
     def get_widget_attrs(self, bound_field):
         attrs = super(SubscribeForm, self).get_widget_attrs(bound_field)
         attrs['ng-model-options'] = "{ debounce : { 'default' : 500, blur : 0 } }"
         return attrs
-
+    
     def clean(self):
         if self.cleaned_data.get('first_name') == 'John' and self.cleaned_data.get('last_name') == 'Doe':
             raise ValidationError('The full name "John Doe" is rejected by the server.')
