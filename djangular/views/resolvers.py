77bac48f09f3e6969889f66ee71381a2b985a0de
Jakob Košir <kosir.jakob@gmail.com>
2015-02-09 11:09:26 +0100

    resolver changes, add middleware processing

--- djangular/views/resolvers.py
+++ djangular/views/resolvers.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+from django.conf import settings
+from django.utils.decorators import decorator_from_middleware
+from django.utils.module_loading import import_string
 from django.views.generic import View
-from django.views.decorators.csrf import csrf_exempt, csrf_protect
+from django.views.decorators.csrf import csrf_exempt
 from django.core.urlresolvers import resolve, reverse
 
 
@@ -13,7 +16,8 @@ class UrlResolverView(View):
         Returns the result of resolved view function, called with provided args and kwargs
 
         Using the resolver for a csrf_exempt view would still raise CSRF verification error on UrlResolverView,
-        thus the resolver must be csrf_exempt. CSRF verification is done on resolved view, if it isn't csrf_exempt
+        thus the resolver must be csrf_exempt. CSRF/Session/Auth and other middleware checks are done when calling
+        the actual view.
         """
 
         url_name = request.GET.get('djng_url_name')
@@ -26,8 +30,7 @@ class UrlResolverView(View):
 
         view, args, kwargs = resolve(reverse(url_name, args=url_args, kwargs=url_kwargs))
 
-        # Return csrf_protected view if it isn't csrf_exempt
-        if hasattr(view, 'csrf_exempt'):
-            if view.csrf_exempt:
-                return view(request, *args, **kwargs)
-        return csrf_protect(view)(request, *args, **kwargs)
+        # Run through all the middlewares when calling actual view
+        for middleware_path in settings.MIDDLEWARE_CLASSES:
+            view = decorator_from_middleware(import_string(middleware_path))(view)
+        return view(request, *args, **kwargs)
