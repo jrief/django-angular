5e43f437b7b8c4d7353d75eb1fe32c441c916eef
Jacob Rief <jacob.rief@gmail.com>
2015-03-05 20:20:01 +0100

    using package.json to keep data DRY

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 import os
+import json
 from setuptools import setup, find_packages
-from djangular import __version__
-
-DESCRIPTION = 'Let Django play well with AngularJS'
 
 CLASSIFIERS = [
     'Environment :: Web Environment',
@@ -17,22 +15,27 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.3',
 ]
 
+
 def read(fname):
     readme_file = os.path.join(os.path.dirname(__file__), fname)
     return os.popen('[ -x "$(which pandoc 2>/dev/null)" ] && pandoc -t rst {0} || cat {0}'.format(readme_file)).read()
 
+with open('package.json', 'r') as fp:
+    package = json.load(fp)
+
 setup(
-    name='django-angular',
-    version=__version__,
-    author='Jacob Rief',
-    author_email='jacob.rief@gmail.com',
-    description=DESCRIPTION,
+    name=package['name'],
+    version=package['version'],
+    author=package['author']['name'],
+    author_email=package['author']['email'],
+    description=package['description'],
     long_description=read('README.md'),
-    url='https://github.com/jrief/django-angular',
-    license='MIT',
-    keywords=['django', 'angularjs'],
+    url=package['homepage'],
+    license=package['license'],
+    keywords=package['keywords'],
     platforms=['OS Independent'],
     classifiers=CLASSIFIERS,
     packages=find_packages(exclude=['examples', 'docs']),
     include_package_data=True,
+    zip_safe=False,
 )
