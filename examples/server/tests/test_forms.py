ace9760125090152d1ae05ae0fbcf7dc9ac7d86e
Andrey Oparin <andrey@edsd.com>
2015-07-28 11:09:30 +0700

    Clean import

--- examples/server/tests/test_forms.py
+++ examples/server/tests/test_forms.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import copy
-from django.contrib.auth.forms import AuthenticationForm
 from django.db import models
 from django import forms
 from django.http import QueryDict
