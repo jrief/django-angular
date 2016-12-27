59e7e19c6d626586f44c013c7074622a7e85a826
Adrien Brunet <adrien@emencia.com>
2016-12-27 20:37:01 +0100

    Feature: Add label as placeholders. (from issue #66)

--- djng/styling/bootstrap/forms.py
+++ djng/styling/bootstrap/forms.py
@@ -13,6 +13,17 @@ class BootstrapFormMixin(object):
     error_row = '<div class="djng-line-spreader">%s</div>'
     normal_row = '<div%(html_class_attr)s>%(label)s%(field)s%(help_text)s%(errors)s</div>'
     row_ender = '</div>'
+    label_as_placeholder = False
+
+    def get_widget_attrs(self, bound_field):
+        """
+        label_as_placeholder allows you to use the placeholder as a label
+        reusing your label definition.
+        """
+        attrs = super(BootstrapFormMixin, self).get_widget_attrs(bound_field)
+        if self.label_as_placeholder:
+            attrs["placeholder"] = bound_field.label
+        return attrs
 
     def as_div(self):
         """
