c3ca3bcc66a5f85fbc3c1cb8e39d3cd91c820f30
Jacob Rief <jacob.rief@gmail.com>
2017-06-30 12:16:20 +0200

    use Beautifulsoup for testing

--- examples/server/tests/settings.py
+++ examples/server/tests/settings.py
@@ -54,6 +54,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'client', 'src'),
 )
 
+FORM_RENDERER = 'djng.forms.renderers.DjangoAngularTemplates'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
