b88a4f918091bbd72130fef098066d912b2f1130
Jacob Rief <jacob.rief@gmail.com>
2017-07-27 14:04:09 +0200

    do not override widgets in Django-1.11

--- djng/forms/widgets.py
+++ djng/forms/widgets.py
@@ -19,94 +19,95 @@ from djng import app_settings
 
 DJANGO_VERSION = get_version()
 
+if LooseVersion(DJANGO_VERSION) < LooseVersion('1.11'):
 
-class ChoiceFieldRenderer(widgets.ChoiceFieldRenderer):
-    def render(self):
+    class ChoiceFieldRenderer(widgets.ChoiceFieldRenderer):
+        def render(self):
+            """
+            Outputs a <ul ng-form="name"> for this set of choice fields to nest an ngForm.
+            """
+            start_tag = format_html('<ul {0}>', mark_safe(' '.join(self.field_attrs)))
+            output = [start_tag]
+            for widget in self:
+                output.append(format_html('<li>{0}</li>', force_text(widget)))
+            output.append('</ul>')
+            return mark_safe('\n'.join(output))
+
+
+    class CheckboxChoiceInput(widgets.CheckboxChoiceInput):
+        def tag(self, attrs=None):
+            attrs = attrs or self.attrs
+            name = '{0}.{1}'.format(self.name, self.choice_value)
+            tag_attrs = dict(attrs, type=self.input_type, name=name, value=self.choice_value)
+            if 'ng-model' in attrs:
+                tag_attrs['ng-model'] = "{0}['{1}']".format(attrs['ng-model'], self.choice_value)
+            if self.is_checked():
+                tag_attrs['checked'] = 'checked'
+            return format_html('<input{0} />', flatatt(tag_attrs))
+
+
+    class CheckboxFieldRendererMixin(object):
+        def __init__(self, name, value, attrs, choices):
+            attrs.pop('djng-error', None)
+            self.field_attrs = [format_html('ng-form="{0}"', name)]
+            #if attrs.pop('multiple_checkbox_required', False):
+            field_names = [format_html('{0}.{1}', name, choice) for choice, dummy in choices]
+            self.field_attrs.append(format_html('validate-multiple-fields="{0}"', json.dumps(field_names)))
+            super(CheckboxFieldRendererMixin, self).__init__(name, value, attrs, choices)
+
+
+    class CheckboxFieldRenderer(CheckboxFieldRendererMixin, ChoiceFieldRenderer):
+        choice_input_class = CheckboxChoiceInput
+
+
+    class CheckboxSelectMultiple(widgets.CheckboxSelectMultiple):
         """
-        Outputs a <ul ng-form="name"> for this set of choice fields to nest an ngForm.
+        Form fields of type 'MultipleChoiceField' using the widget 'CheckboxSelectMultiple' must behave
+        slightly different from the original. This widget overrides the default functionality.
         """
-        start_tag = format_html('<ul {0}>', mark_safe(' '.join(self.field_attrs)))
-        output = [start_tag]
-        for widget in self:
-            output.append(format_html('<li>{0}</li>', force_text(widget)))
-        output.append('</ul>')
-        return mark_safe('\n'.join(output))
+        renderer = CheckboxFieldRenderer
+
+        def id_for_label(self, id_):
+            """
+            Returns the label for the group of checkbox input fields
+            """
+            return id_
+
+        def implode_multi_values(self, name, data):
+            raise NotImplementedError("This method has been moved to its FieldMixin.")
+
+        def convert_ajax_data(self, field_data):
+            raise NotImplementedError("This method has been moved to its FieldMixin.")
+
+        def get_field_attrs(self, field):
+            raise NotImplementedError("This method has been moved to its FieldMixin.")
 
 
-class CheckboxChoiceInput(widgets.CheckboxChoiceInput):
-    def tag(self, attrs=None):
-        attrs = attrs or self.attrs
-        name = '{0}.{1}'.format(self.name, self.choice_value)
-        tag_attrs = dict(attrs, type=self.input_type, name=name, value=self.choice_value)
-        if 'ng-model' in attrs:
-            tag_attrs['ng-model'] = "{0}['{1}']".format(attrs['ng-model'], self.choice_value)
-        if self.is_checked():
-            tag_attrs['checked'] = 'checked'
-        return format_html('<input{0} />', flatatt(tag_attrs))
+    class RadioFieldRendererMixin(object):
+        def __init__(self, name, value, attrs, choices):
+            attrs.pop('djng-error', None)
+            self.field_attrs = []
+            if attrs.pop('radio_select_required', False):
+                self.field_attrs.append(format_html('validate-multiple-fields="{0}"', name))
+            super(RadioFieldRendererMixin, self).__init__(name, value, attrs, choices)
 
 
-class CheckboxFieldRendererMixin(object):
-    def __init__(self, name, value, attrs, choices):
-        attrs.pop('djng-error', None)
-        self.field_attrs = [format_html('ng-form="{0}"', name)]
-        #if attrs.pop('multiple_checkbox_required', False):
-        field_names = [format_html('{0}.{1}', name, choice) for choice, dummy in choices]
-        self.field_attrs.append(format_html('validate-multiple-fields="{0}"', json.dumps(field_names)))
-        super(CheckboxFieldRendererMixin, self).__init__(name, value, attrs, choices)
+    class RadioFieldRenderer(RadioFieldRendererMixin, ChoiceFieldRenderer):
+        choice_input_class = widgets.RadioChoiceInput
 
 
-class CheckboxFieldRenderer(CheckboxFieldRendererMixin, ChoiceFieldRenderer):
-    choice_input_class = CheckboxChoiceInput
-
-
-class CheckboxSelectMultiple(widgets.CheckboxSelectMultiple):
-    """
-    Form fields of type 'MultipleChoiceField' using the widget 'CheckboxSelectMultiple' must behave
-    slightly different from the original. This widget overrides the default functionality.
-    """
-    renderer = CheckboxFieldRenderer
-
-    def id_for_label(self, id_):
+    class RadioSelect(widgets.RadioSelect):
         """
-        Returns the label for the group of checkbox input fields
+        Form fields of type 'ChoiceField' using the widget 'RadioSelect' must behave
+        slightly different from the original. This widget overrides the default functionality.
         """
-        return id_
+        renderer = RadioFieldRenderer
 
-    def implode_multi_values(self, name, data):
-        raise NotImplementedError("This method has been moved to its FieldMixin.")
+        def id_for_label(self, id_):
+            return id_
 
-    def convert_ajax_data(self, field_data):
-        raise NotImplementedError("This method has been moved to its FieldMixin.")
-
-    def get_field_attrs(self, field):
-        raise NotImplementedError("This method has been moved to its FieldMixin.")
-
-
-class RadioFieldRendererMixin(object):
-    def __init__(self, name, value, attrs, choices):
-        attrs.pop('djng-error', None)
-        self.field_attrs = []
-        if attrs.pop('radio_select_required', False):
-            self.field_attrs.append(format_html('validate-multiple-fields="{0}"', name))
-        super(RadioFieldRendererMixin, self).__init__(name, value, attrs, choices)
-
-
-class RadioFieldRenderer(RadioFieldRendererMixin, ChoiceFieldRenderer):
-    choice_input_class = widgets.RadioChoiceInput
-
-
-class RadioSelect(widgets.RadioSelect):
-    """
-    Form fields of type 'ChoiceField' using the widget 'RadioSelect' must behave
-    slightly different from the original. This widget overrides the default functionality.
-    """
-    renderer = RadioFieldRenderer
-
-    def id_for_label(self, id_):
-        return id_
-
-    def get_field_attrs(self, field):
-        raise NotImplementedError("This method has been moved to its FieldMixin.")
+        def get_field_attrs(self, field):
+            raise NotImplementedError("This method has been moved to its FieldMixin.")
 
 
 class DropFileWidget(widgets.Widget):
