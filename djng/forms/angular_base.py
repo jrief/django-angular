7663d52efb76e485ff8bf5447ca41797fe11c5c9
Austin Snoeyink <asnoeyink@edustaff.org>
2017-08-25 08:15:05 -0400

    Make sure NgWidgetMixin isn't added to the widget more than once.

--- djng/forms/angular_base.py
+++ djng/forms/angular_base.py
@@ -204,7 +204,9 @@ class NgBoundField(forms.BoundField):
         if DJANGO_VERSION > (1, 10):
             # so that we can refer to the field when building the rendering context
             widget._field = self.field
-            widget.__class__ = type(widget.__class__.__name__, (NgWidgetMixin, widget.__class__), {})
+            # Make sure that NgWidgetMixin is not already part of the widget's bases so it doesn't get added twice.
+            if not isinstance(widget, NgWidgetMixin):
+                widget.__class__ = type(widget.__class__.__name__, (NgWidgetMixin, widget.__class__), {})
         return super(NgBoundField, self).as_widget(widget, attrs, only_initial)
 
     def build_widget_attrs(self, attrs, widget=None):
