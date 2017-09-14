92dfb6802046768d46ba3173aaa6934f2bcbb8fd
Jacob Rief <jacob.rief@gmail.com>
2017-09-15 00:28:31 +0200

    working prototype for formsets

--- examples/server/views/forms_set.py
+++ examples/server/views/forms_set.py
@@ -2,6 +2,9 @@
 from __future__ import unicode_literals
 from server.forms.forms_set import SubscribeForm, AddressForm
 # start tutorial
+import json
+import time
+from django.http import JsonResponse, HttpResponseBadRequest
 from django.views.generic import TemplateView
 from django.core.urlresolvers import reverse_lazy
 
@@ -10,8 +13,23 @@ class SubscribeView(TemplateView):
     template_name = 'forms-set.html'
     success_url = reverse_lazy('form_data_valid')
 
-    def get_context_data(self, **kwargs):
-        context = super(SubscribeView, self).get_context_data(**kwargs)
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
         context['subscribe_form'] = SubscribeForm()
         context['address_form'] = AddressForm()
-        return context
+        return self.render_to_response(context)
+
+    def put(self, request, *args, **kwargs):
+        form_data = json.loads(request.body)
+        time.sleep(form_data.get('delay', 0) / 1000.)
+        subscribe_form = SubscribeForm(data=form_data.get(SubscribeForm.scope_prefix, {}))
+        address_form = AddressForm(data=form_data.get(AddressForm.scope_prefix, {}))
+        if subscribe_form.is_valid() and address_form.is_valid():
+            return JsonResponse({'success_url': self.success_url})
+
+        # report errors
+        response_data = {
+            subscribe_form.form_name: {'errors': subscribe_form.errors},
+            address_form.form_name: {'errors': address_form.errors},
+        }
+        return HttpResponseBadRequest(json.dumps(response_data), status=422, content_type='application/json')
