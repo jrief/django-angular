ce3e7cdf1c26ecb6af1dfa24efe319ecf3b29686
Jacob Rief <jacob.rief@gmail.com>
2017-09-27 13:10:22 +0200

    simplified li_format

--- djng/forms/angular_base.py
+++ djng/forms/angular_base.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 
 import json
 from base64 import b64encode
-from types import MethodType
 
 try:
     from collections import UserList
@@ -52,9 +51,6 @@ class TupleErrorList(UserList, list):
     5: The desired error message. If this contains the magic word '$message' it will be added with
        ``ng-bind`` rather than rendered inside the list item.
     """
-    li_format = '<li ng-show="{0}.{1}" class="{2}">{3}</li>'
-    li_format_bind = '<li ng-show="{0}.{1}" class="{2}" ng-bind="{0}.{3}"></li>'
-
     def __init__(self, initlist=None, error_class=None):
         super(TupleErrorList, self).__init__(initlist)
 
@@ -99,7 +95,10 @@ class TupleErrorList(UserList, list):
         if isinstance(first, tuple):
             error_lists = {'$pristine': [], '$dirty': []}
             for e in self:
-                li_format = e[5] == '$message' and self.li_format_bind or self.li_format
+                if e[5] == '$message':
+                    li_format = '<li ng-show="{0}.{1}" class="{2}" ng-bind="{0}.{3}"></li>'
+                else:
+                    li_format = '<li ng-show="{0}.{1}" class="{2}">{3}</li>'
                 err_tuple = (e[0], e[3], e[4], force_text(e[5]))
                 error_lists[e[2]].append(format_html(li_format, *err_tuple))
             # renders and combine both of these lists
