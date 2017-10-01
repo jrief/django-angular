4a904b943ecf56aca97e17d9abbe53220f2a6d50
Jacob Rief <jacob.rief@gmail.com>
2017-10-01 16:35:25 +0200

    added function fetch to button actions

--- examples/server/views/combined_validation.py
+++ examples/server/views/combined_validation.py
@@ -13,21 +13,21 @@ class SubscribeView(FormView):
     form_class = SubscribeForm
     success_url = reverse_lazy('form_data_valid')
 
+    def get(self, request, **kwargs):
+        if request.is_ajax():
+            form = self.form_class(initial=default_subscribe_data)
+            return JsonResponse({form.form_name: form.initial})
+        return super(SubscribeView, self).get(request, **kwargs)
+
     def post(self, request, **kwargs):
         assert request.is_ajax()
         return self.ajax(request)
 
     def ajax(self, request):
         request_data = json.loads(request.body)
-        set_defaults = request_data.pop('set_defaults', False)
-        if set_defaults:
-            form = self.form_class(initial=default_subscribe_data)
-        else:
-            form = self.form_class(data=request_data.get(self.form_class.scope_prefix, {}))
+        form = self.form_class(data=request_data.get(self.form_class.scope_prefix, {}))
         if form.is_valid():
             return JsonResponse({'success_url': force_text(self.success_url)})
-        elif set_defaults:
-            return JsonResponse({form.form_name: form.initial})
         else:
             response_data = {form.form_name: form.errors}
             return HttpResponseBadRequest(json.dumps(response_data), status=422, content_type='application/json')
