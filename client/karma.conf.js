24eb626dab53682001930e723a5a1aa0f0da65aa
Jacob Rief <jacob.rief@gmail.com>
2017-10-13 15:15:07 +0200

    Bump to 0.8.6

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -8,8 +8,8 @@ module.exports = function(config) {
 
 		// list of files / patterns to load in the browser
 		files: [
-			'bower_components/angular/angular.js',
-			'bower_components/angular-mocks/angular-mocks.js',
+			'node_modules/angular/angular.js',
+			'node_modules/angular-mocks/angular-mocks.js',
 			'src/js/*.js',
 			'tests/*.js'
 		],
--- djng/__init__.py
+++ djng/__init__.py
@@ -20,4 +20,4 @@ Release logic:
 13. git push
 """
 
-__version__ = '0.8.5'
+__version__ = '0.8.6'
