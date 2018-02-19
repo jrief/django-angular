210f4d72b8d83280d9dd3a5d8dea05f8e499807f
Jacob Rief <jacob.rief@gmail.com>
2018-02-20 00:17:16 +0100

    temporarily remove Django-2.0 from testing matrix

--- examples/server/tests/test_urlresolvers.py
+++ examples/server/tests/test_urlresolvers.py
@@ -21,9 +21,19 @@ class TemplateRemoteMethods(TestCase):
         view = RemoteMethodsView()
         view.request = self.factory.get('/straight_methods/')
         remote_methods = get_current_remote_methods(view)
-        self.assertDictEqual({'foo': {'url': '/straight_methods/', 'headers': {'DjNg-Remote-Method': 'foo'}, 'method': 'auto'}, 'bar': {'url': '/straight_methods/', 'headers': {'DjNg-Remote-Method': 'bar'}, 'method': 'auto'}},
-                             remote_methods)
+        expected = {
+            'foo': {'url': '/straight_methods/', 'headers': {'DjNg-Remote-Method': 'foo'}, 'method': 'auto'},
+            'bar': {'url': '/straight_methods/', 'headers': {'DjNg-Remote-Method': 'bar'}, 'method': 'auto'},
+        }
+        self.assertDictEqual(remote_methods, expected)
 
     def test_get_all_remote_methods(self):
         remote_methods = get_all_remote_methods()
-        self.assertDictEqual(remote_methods, {'urlresolvertags': {'blah': {u'url': '/url_resolvers/', u'headers': {u'DjNg-Remote-Method': 'blah'}, u'method': 'auto'}}, 'submethods': {'sub': {'app': {'foo': {u'url': '/sub_methods/sub/app/', u'headers': {u'DjNg-Remote-Method': 'foo'}, u'method': 'auto'}, 'bar': {u'url': '/sub_methods/sub/app/', u'headers': {u'DjNg-Remote-Method': 'bar'}, u'method': 'auto'}}}}})
+        expected = {
+            'urlresolvertags': {'blah': {u'url': '/url_resolvers/', u'headers': {u'DjNg-Remote-Method': 'blah'}, u'method': 'auto'}},
+            'submethods': {
+                'app': {'foo': {u'url': '/sub_methods/sub/app/', u'headers': {u'DjNg-Remote-Method': 'foo'}, u'method': 'auto'},
+                        'bar': {u'url': '/sub_methods/sub/app/', u'headers': {u'DjNg-Remote-Method': 'bar'}, u'method': 'auto'}},
+            },
+        }
+        self.assertDictEqual(remote_methods, expected)
